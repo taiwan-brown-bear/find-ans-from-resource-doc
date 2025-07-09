03390f83681294b1bbeaeff34c9deb3481a66c03
taiwan-brown-bear <charles.liu.0857@gmail.com>
2025-07-08 18:21:24 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# find-ans-from-resource-doc
---
+++ src/main/java/com/taiwan_brown_bear/find_ans_from_resource_doc/FindAnsFromResourceDocApplication.java
@@ -0,0 +1,13 @@
+package com.taiwan_brown_bear.find_ans_from_resource_doc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindAnsFromResourceDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindAnsFromResourceDocApplication.class, args);
+	}
+
+}
