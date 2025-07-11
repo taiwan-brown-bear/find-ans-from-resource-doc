f5c8f6f62d08809d33dec9e90a1f2d9365ee2fed
taiwan-brown-bear <charles.liu.0857@gmail.com>
2025-07-10 18:32:32 -0700

    enhancement: only load model once

--- src/main/java/com/taiwan_brown_bear/find_ans_from_resource_doc/service/FindAnsFromResourceDocService.java
+++ src/main/java/com/taiwan_brown_bear/find_ans_from_resource_doc/service/FindAnsFromResourceDocService.java
@@ -1,8 +1,10 @@
 package com.taiwan_brown_bear.find_ans_from_resource_doc.service;
 
+import ai.djl.MalformedModelException;
 import ai.djl.inference.Predictor;
 import ai.djl.modality.nlp.qa.QAInput;
 import ai.djl.repository.zoo.Criteria;
+import ai.djl.repository.zoo.ModelNotFoundException;
 import ai.djl.repository.zoo.ZooModel;
 import ai.djl.training.util.ProgressBar;
 import com.taiwan_brown_bear.find_ans_from_resource_doc.configuration.FindAnsFromResourceDocConfiguration;
@@ -12,54 +14,55 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @Slf4j
 @Service
 public class FindAnsFromResourceDocService {
 
-    private FindAnsFromResourceDocConfiguration  conf;
     private FindAnsFromResourceDocInitialization init;
     private FindAnsFromResourceDocBertTranslator translator;
+    private ZooModel                             model;
 
-    public FindAnsFromResourceDocService(FindAnsFromResourceDocConfiguration conf) throws IOException {
-        this.conf = conf;
-        log.info("Initializing / Downloading ...");
-        this.init = new FindAnsFromResourceDocInitialization(conf);
+    public FindAnsFromResourceDocService(FindAnsFromResourceDocConfiguration conf) throws IOException, MalformedModelException, ModelNotFoundException {
+        this.init       = new FindAnsFromResourceDocInitialization(conf);
         this.translator = new FindAnsFromResourceDocBertTranslator(conf);
+        this.model      = getModel(Paths.get(conf.getLOCAL_VOCABULARY_PYTORCH_MODEL_DIRECTORY_URL()), translator);
     }
 
     public String findAnswer(String providedResourceDoc, String question)
     {
-        try
-        {
-            // Note: use criteria to get model and,
-            //       then, use model to get predictor.
-            //       both criteria and predictor need translator.
-            //
-            //FindAnsFromResourceDocBertTranslator translator = new FindAnsFromResourceDocBertTranslator();
-
-            Criteria<QAInput, String> modelConfigAndSearchCriteria = Criteria.builder()
-                    .setTypes(QAInput.class, String.class)
-                    .optModelPath(Paths.get(conf.getLOCAL_VOCABULARY_PYTORCH_MODEL_DIRECTORY_URL()))// search this local folder
-                    .optTranslator(translator)
-                    .optProgress(new ProgressBar()).build();
-
-            log.info("start downloading the model ...");
-            ZooModel deserializedPytorchModel = modelConfigAndSearchCriteria.loadModel();
-            log.info("finished downloading the model ...");
-
-            try (Predictor<QAInput, String> predictor = deserializedPytorchModel.newPredictor(translator)) {
+        try {
+            try (Predictor<QAInput, String> predictor = model.newPredictor(translator)) {
                 QAInput input = new QAInput(question, providedResourceDoc);
                 String answer = predictor.predict(input);
                 log.info("QAInput has Question as \"{}\" & Resource Document as \"{}\"", input.getQuestion(), input.getParagraph() );
                 log.info("Answer  is \"{}\"", answer);
                 return answer;
             }
-
         } catch (Exception e) {
             log.error("Failed to answer due to {}", e.getMessage(), e);
         }
         return null;
+    }
+
+    private ZooModel getModel(Path modelPath, FindAnsFromResourceDocBertTranslator translator)
+            throws
+            IOException,
+            ai.djl.repository.zoo.ModelNotFoundException,
+            ai.djl.MalformedModelException {
+
+        // Note: use criteria to get model and,
+        //       then, use model to get predictor.
+        //       both criteria and predictor need translator.
+        //
+        Criteria<QAInput, String> modelConfigAndSearchCriteria = Criteria.builder()
+                .setTypes(QAInput.class, String.class)
+                .optModelPath(modelPath)// search this local folder
+                .optTranslator(translator)
+                .optProgress(new ProgressBar()).build();
+
+        return modelConfigAndSearchCriteria.loadModel();
     }
 }
